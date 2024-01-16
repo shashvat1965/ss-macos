d48f695c6d687168b36cf0e1779453e66fe76f05
shashvat1965 <shashvat1965@gmail.com>
2024-01-16 21:19:26 +0530

    Initial Commit

---
+++ screencapture-clueso/screencapture_cluesoApp.swift
@@ -0,0 +1,17 @@
+//
+//  screencapture_cluesoApp.swift
+//  screencapture-clueso
+//
+//  Created by shashvat singh on 16/01/24.
+//
+
+import SwiftUI
+
+@main
+struct screencapture_cluesoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
