6c0e0c845776cac4472858578ceb1b90f7ff696d
shashvat1965 <shashvat1965@gmail.com>
2024-01-17 02:42:38 +0530

    screenshot done

--- screencapture-clueso/ContentView.swift
+++ screencapture-clueso/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var image = NSImage();
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Screen Shot"){
+                if screenshotWindowAndSuccess() {
+                    image = getImageFromPasteboard();
+                }
+            }
+            .padding()
+            Image(nsImage: image)
+                .padding()
         }
         .padding()
+    }
+    
+
+    func screenshotWindowAndSuccess() -> Bool {
+        let task = Process();
+        task.launchPath = "/usr/sbin/screencapture";
+        task.arguments = ["-vIu"];
+        task.launch();
+        task.waitUntilExit();
+        let status = task.terminationStatus;
+        return status == 0;
+    }
+    
+    func getImageFromPasteboard() -> NSImage {
+        let pasteboard = NSPasteboard.general;
+        guard pasteboard.canReadItem(withDataConformingToTypes:
+                                        NSImage.imageTypes) else { return NSImage(); }
+        guard let image = NSImage(pasteboard: pasteboard) else { return
+            NSImage() };
+        return image;
     }
 }
 
